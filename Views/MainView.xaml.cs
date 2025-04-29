1c5831c6f5f76d2909409a701d54fec9192d8f30
rvn14 <dasunadithya123@gmail.com>
2025-04-29 06:41:34 +0530

    backup save

--- Views/MainView.xaml.cs
+++ Views/MainView.xaml.cs
@@ -37,7 +37,7 @@ namespace HRM_System.Views
             UpdateNavButtonStyles(HomeButton);
             
             // Create and display HomeView
-            ContentArea.Content = new DashboardView();
+            ContentArea.Content = new HomeView();
         }
 
         private void EmployeeButton_Click(object sender, RoutedEventArgs e)
